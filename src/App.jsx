a12e7e9b2bbabcbb96db5d1aebb5dd302e62e507
blakeknees <jenniferblakney@gmail.com>
2024-12-02 16:58:48 -0500

    began CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="container">
         <header>
           <Header />
         </header>  
