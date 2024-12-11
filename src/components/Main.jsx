34879a9d2c438e52d30640e34a189cfe6532a7b7
blakeknees <jenniferblakney@gmail.com>
2024-12-11 12:03:39 -0500

    finally getting somewhere

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,12 @@ const Main = ({}) => {
     
     return (
         <div className="main">
-        <h1>Jennifer Blakney</h1>
-            <div>
-              <p>
-                Web Developer. Cannabis Content Creator.
-              </p>
-            </div>
-
+          <h1>Jennifer Blakney</h1>
+        
+          <p className="descriptors">
+            Web Developer. Cannabis Content Creator. Former Pastry Chef.
+          </p>
+      
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto recusandae doloribus veniam eius fugit, nostrum voluptatibus tempora exercitationem repellat esse accusantium a modi corporis, id, ullam fuga vitae temporibus?
           </p>
