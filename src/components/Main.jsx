8cfefeaccef9fec97ffc68011018529024ac7950
blakeknees <jenniferblakney@gmail.com>
2024-12-15 17:19:15 -0500

    working from a proper clone now and made some needed css fixes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,18 +7,23 @@ const Main = ({}) => {
           <p className="descriptors">
             Web Developer. Cannabis Content Creator. Former Pastry Chef.
           </p>
-      
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto recusandae doloribus veniam eius fugit, nostrum voluptatibus tempora exercitationem repellat esse accusantium a modi corporis, id, ullam fuga vitae temporibus?
-          </p>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto recusandae doloribus veniam eius fugit, nostrum voluptatibus tempora exercitationem repellat esse accusantium a modi corporis, id, ullam fuga vitae temporibus?
-          </p>
+          <img src="https://placebear.com/200/300" alt="" className="selfie"/>
 
-          <p>
-            If you'd like to chat, you can email me at <a href="mailto:jenniferblakney@gmail.com">jenniferblakney@gmail.com</a>
-          </p>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto recusandae doloribus veniam eius fugit, nostrum voluptatibus tempora exercitationem repellat esse accusantium a modi corporis, id, ullam fuga vitae temporibus?
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto recusandae doloribus veniam eius fugit, nostrum voluptatibus tempora exercitationem repellat esse accusantium a modi corporis, id, ullam fuga vitae temporibus?
+            </p>
+
+            <p>
+              If you'd like to chat, you can email me at <a href="mailto:jenniferblakney@gmail.com">jenniferblakney@gmail.com</a>
+            </p>
+          </div>
+
         </div>
     )
 }
