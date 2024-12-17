92cb6a657502e5d041150cbd0daaf5d45f5ee59e
blakeknees <jenniferblakney@gmail.com>
2024-12-17 17:02:39 -0500

    background image working now

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import $cannabis from './assets/cannabis-background.jpg';
 
 
 function App() {
