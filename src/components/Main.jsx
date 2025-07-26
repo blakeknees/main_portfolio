5ce9cd59b6ab8025ee4553302ee4bf6b77ac5aef
blakeknees <jenniferblakney@gmail.com>
2025-07-26 15:40:53 -0400

    more bio

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,11 +12,15 @@ const Main = ({}) => {
 
           <div>
             <p>
-              I have been involved in the cannabis industry and community since 2016. Using my Le Cordon Bleu pastry knowledge, I began my self-education journey by making edibles for myself and friends. With lots of encouragement I submitted my best product, an infused lemon macaron, to The Karma Cup competition. I won 2nd place.
+              I have been involved in the cannabis industry and community since 2016. Using my Le Cordon Bleu pastry knowledge, I began my self-education journey by making edibles for myself and friends. With lots of encouragement I submitted my best product, an infused lemon macaron, to The sKarma Cup competition. I won 2nd place.
             </p>
 
             <p>
-              In 2017 I began working in medical cannabis clinics, helping patients get access and educating them on products.I took that love of education and soon began writing for various publications, including the OCS and Cannabis Retailer Magazine.
+              From 2017-2020 I worked in medical cannabis clinics, helping patients obtain prescriptions and educating them on products. My expansive knowledge and writing experience gained me opportunities publishing articles for many cannabis publications and companies, including the OCS and Cannabis Retailer Magazine.
+            </p>
+
+            <p>
+              
             </p>
 
             <p>
