99e65d395b02127685410bb916f8e6b22e3fc6b2
blakeknees <jenniferblakney@gmail.com>
2025-08-03 16:27:24 -0400

    filling out pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,25 @@
 function About() {
-  return <h1>Welcome to the Home Page</h1>;
+  return (<div className="main">
+          <h1>Welcome to my lore...</h1>
+          <div>
+            <p>
+              I have been involved in the cannabis industry and community since 2016. Using my Le Cordon Bleu pastry knowledge, I began my self-education journey by making edibles for myself and friends. With lots of encouragement I submitted my best product, an infused lemon macaron, to The sKarma Cup competition. I won 2nd place.
+            </p>
+
+            <p>
+              From 2017-2020 I worked in medical cannabis clinics, helping patients obtain prescriptions and educating them on products. Using what I learned in clinics, I turned to my experience as a writer to publish articles and email marketing for many cannabis publications and companies, including the OCS, Cannabis Retailer Magazine, Tremblant and Emblem.
+            </p>
+
+            <p>
+             Recently I have moved away from working in cannabis full-time, but I still love creating content about it on the internet. Specifically breaking the stigma that you can't be productive and use cannabis (follow me on Instagram to learn more).
+            </p>
+
+            <p>
+              If you'd like to chat about the industry or collaborate, you can email me at <a href="mailto:jenniferblakney@gmail.com">jenniferblakney@gmail.com</a>
+            </p>
+          </div>
+
+        </div>)
 }
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
 function Home() {
-  return <h1>Welcome to the Home Page</h1>;
+  return (<div className="main">
+          <h1>Jennifer Blakney</h1>
+        
+          <p className="descriptors">
+            Cannabis Writer & Content Creator
+          </p>
+
+          <img src="https://placebear.com/200/300" alt="" className="selfie"/>
+
+          <div>
+            <p>
+              Welcome! Here you can learn about my journey in the cannabis industry, my professional contributions to it, and the media I've been included in.
+            </p>
+
+            <p>
+              If you'd like to chat about the industry or collaborate, you can email me at <a href="mailto:jenniferblakney@gmail.com">jenniferblakney@gmail.com</a>
+            </p>
+          </div>
+
+        </div>)
 }
 
 export default Home;
