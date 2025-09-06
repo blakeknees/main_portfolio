4ab24aa7c6bcddbea9ac48dc57ff3dc1ea686407
blakeknees <jenniferblakney@gmail.com>
2025-09-06 17:17:29 -0400

    media styles

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -7,6 +7,13 @@ function Media() {
             </p>
 
             <h2>Articles</h2>
+              <div className="articles">
+                <a href="https://ocs.ca/blogs/cannabis-made-clear/what-s-changed-after-five-years-of-legal-cannabis-in-canada">
+                  <img src="https://placebear.com/200/300" alt="" />
+                  <h3>Ontario Cannabis Store</h3>
+                  <p>"What's Changed After Five Years of Legal Cannabis in Canada"</p>
+                </a>
+              </div>
             
             <h2>Videos</h2>
           </div>
